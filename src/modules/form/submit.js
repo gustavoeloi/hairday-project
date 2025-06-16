db18c943b3450680a1d5a74426e4a41b488d71a7
Gustavo Eloi <gustavooeloi@gmail.com>
2025-06-16 12:05:04 -0300

    formatting a data to API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 const form = document.querySelector("form");
 const selectedDate = document.getElementById("date");
+const clientName = document.getElementById("client");
 
 const todayRef = dayjs(new Date()).format("YYYY-MM-DD");
 
@@ -14,5 +15,34 @@ selectedDate.min = todayRef;
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  console.log("Rodando o Script");
+  try {
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert("Informe o nome do cliente");
+    }
+
+    const hourSelected = document.querySelector(".hour-selected");
+
+    if (!hourSelected) {
+      return alert("Selecione o horário");
+    }
+
+    const [hour] = hourSelected.innerText.split(":");
+
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    // generate a ID
+
+    const id = new Date().getTime();
+
+    console.log({
+      id,
+      name,
+      when,
+    });
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento");
+    console.log(error);
+  }
 };
